70858b6a66f7fcdd2b1cf677931ae6be0868282e
YummyJOJO <568225176@qq.com>
2022-07-17 18:32:16 +0800

    TRYmoreOnce

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -9,6 +9,9 @@ public class LeapYear {
     public static boolean isLeapYear(int year){
         return ((year%4==0&&year%100!=0)||year%400==0);
         /**add some comment
+         * 
+         * end
+         * /
     }
     
     private static void checkLeapYear(int year) {
