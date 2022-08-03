36d8386ce46c714fdade75bf9b458f539ba7f7ea
YummyJOJO <568225176@qq.com>
2022-08-03 22:52:23 +0800

    Lab2FristSubmit

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,15 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null){
+            return  B;
+        }
+        IntList passtonext = A;
+        while(passtonext != null){
+            passtonext = A.rest;
+        }
+        passtonext.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +99,15 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null){
+            return  B;
+        }
+
+        if(A.rest == null ){
+            return new IntList(A.first, B);
+        }
+        return new IntList(A.first, catenate(A.rest, B));
+
     }
 
 
